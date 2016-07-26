5bb839497a29fdaaf37c3e6f2d3c2c52cd7d59cb
philipdenlinger <philipdenlinger@gmail.com>
2016-07-26 09:07:24 -0400

    base build of mcmaurora theme

---
+++ js/tablefix.js
@@ -0,0 +1,12 @@
+(function ($) {
+    //add drupal 7 code
+    Drupal.behaviors.myfunction = {
+        attach: function(context, settings) {
+//end drupal calls
+$('tr').each( function () {
+   $(this).children('td:gt(0)').filter(function() {
+         return this.innerHTML.match(/^[0-9\s\.,\$\-%±\–]+$/);
+   }).css('text-align','right')
+});
+  }}})
+(jQuery);
